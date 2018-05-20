648c5c7dc619a1ef26d3caf8e628d22e44cfed38
cristianmmuresan <cristian@wearewip.com>
2018-05-20 15:00:35 +0300

    recipes layout for tablets

--- app/src/main/java/com/example/android/bakingtime/main/MainActivity.java
+++ app/src/main/java/com/example/android/bakingtime/main/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.android.bakingtime.main;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.widget.Toast;
 
 import com.example.android.bakingtime.R;
@@ -34,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private void initRecyclerView() {
         recipesAdapter = new RecipesAdapter(this);
 
-        binding.recipesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        binding.recipesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.recipesRecyclerView.setHasFixedSize(true);
         binding.recipesRecyclerView.setAdapter(recipesAdapter);
     }
