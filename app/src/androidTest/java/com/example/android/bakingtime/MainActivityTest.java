9d136afc9a2e17946ffb55f653833171707e8f5a
cristianmmuresan <cristian@wearewip.com>
2018-05-23 22:19:01 +0300

    espresso testing

---
+++ app/src/androidTest/java/com/example/android/bakingtime/MainActivityTest.java
@@ -0,0 +1,53 @@
+package com.example.android.bakingtime;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.android.bakingtime.main.MainActivity;
+import com.example.android.bakingtime.stepdetails.StepDetailsActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    private static final String FIRST_INGREDIENT = "2.0 cups Graham Cracker crumbs";
+    private static final String RECIPE_NAME = "Nutella Pie";
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickRecipe_LaunchesRecipeDetailsActivity() {
+        onView(withId(R.id.recipes_recycler_view)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(RECIPE_NAME)), click()));
+
+        onView(withId(R.id.ingredients_content)).check(matches(withText(startsWith(FIRST_INGREDIENT))));
+    }
+
+    @Rule
+    public ActivityTestRule<StepDetailsActivity> recipeActivityTestRule
+            = new ActivityTestRule<>(StepDetailsActivity.class);
+
+    @Test
+    public void clickStep_LaunchesStepDetailsActivity() {
+        onView(withId(R.id.recipes_recycler_view)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(RECIPE_NAME)), click()));
+
+        onView(withId(R.id.steps_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.step_previous)).check(matches(not(isDisplayed())));
+    }
+}
