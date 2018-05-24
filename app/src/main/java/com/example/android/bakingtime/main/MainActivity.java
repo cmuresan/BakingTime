1463718a9228da45977a12fd98048a31e468d616
cristianmmuresan <cristian@wearewip.com>
2018-05-24 22:13:03 +0300

    save recycler's position

--- app/src/main/java/com/example/android/bakingtime/main/MainActivity.java
+++ app/src/main/java/com/example/android/bakingtime/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.widget.Toast;
 
 import com.example.android.bakingtime.R;
@@ -13,15 +14,19 @@ import com.example.android.networkmodule.network.ApiImpl;
 import com.example.android.networkmodule.network.ApiInterface;
 import com.example.android.networkmodule.network.CallbackInterface;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String RECIPES_PREFERENCE_NAME = "MainActivity.RECIPES_PREFERENCE_NAME";
     public static final String RECIPES_PREFERENCE_KEY = "MainActivity.RECIPES_PREFERENCE_KEY";
+    private static final String RV_CURRENT_VISIBLE_POSITION = "MainActivity.RV_CURRENT_VISIBLE_POSITION";
     private ActivityMainBinding binding;
     private RecipesAdapter recipesAdapter;
+    private long currentVisiblePosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +35,31 @@ public class MainActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            currentVisiblePosition = savedInstanceState.getLong(RV_CURRENT_VISIBLE_POSITION);
+        }
+
         initRecyclerView();
         getRecipes();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.recipesRecyclerView.getLayoutManager()
+                .scrollToPosition((int) currentVisiblePosition);
+        currentVisiblePosition = 0;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveCurrentVisiblePosition();
+    }
+
+    private void saveCurrentVisiblePosition() {
+        currentVisiblePosition = ((LinearLayoutManager) binding.recipesRecyclerView.getLayoutManager())
+                .findFirstCompletelyVisibleItemPosition();
     }
 
     private void initRecyclerView() {
@@ -39,6 +67,19 @@ public class MainActivity extends AppCompatActivity {
 
         binding.recipesRecyclerView.setHasFixedSize(true);
         binding.recipesRecyclerView.setAdapter(recipesAdapter);
+        getLocallySavedRecipes();
+    }
+
+    private void getLocallySavedRecipes() {
+        SharedPreferences prefs = getSharedPreferences(MainActivity.RECIPES_PREFERENCE_NAME, MODE_PRIVATE);
+        String recipeJson = prefs.getString(MainActivity.RECIPES_PREFERENCE_KEY, null);
+
+        if (recipeJson != null) {
+            Type type = new TypeToken<List<Recipe>>() {
+            }.getType();
+            List<Recipe> recipes = new Gson().fromJson(recipeJson, type);
+            recipesAdapter.setRecipes(recipes);
+        }
     }
 
     private void getRecipes() {
@@ -63,5 +104,12 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor prefs = getSharedPreferences(RECIPES_PREFERENCE_NAME, MODE_PRIVATE).edit();
         prefs.putString(RECIPES_PREFERENCE_KEY, new Gson().toJson(response));
         prefs.apply();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveCurrentVisiblePosition();
+        outState.putLong(RV_CURRENT_VISIBLE_POSITION, currentVisiblePosition);
     }
 }
